cf354a0aca866dee5d53b5ecfe178443c4bec669
khrisnaa <igdkhrisnaaa@gmail.com>
2025-01-23 21:39:08 +0800

    feta: add zoom parallax

---
+++ resources/js/Pages/Client/Partials/ExploreZoomParallax.jsx
@@ -0,0 +1,93 @@
+import React, { useRef } from "react";
+import { useScroll, useTransform, motion } from "motion/react";
+import { cn } from "@/lib/utils";
+
+const ExploreZoomParallax = () => {
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["start start", "end end"],
+    });
+
+    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+    const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+    const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+    const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
+    const opacity = useTransform(scrollYProgress, [0.2, 1], [0, 1]);
+
+    const pictures = [
+        {
+            src: "/assets/images/dummy_hero.jpg",
+            scale: scale4,
+        },
+        {
+            src: "/assets/images/image_3.jpeg",
+            scale: scale6,
+        },
+        {
+            src: "/assets/images/image_3.jpeg",
+            scale: scale6,
+        },
+        {
+            src: "/assets/images/image_3.jpeg",
+            scale: scale5,
+        },
+        {
+            src: "/assets/images/image_3.jpeg",
+            scale: scale6,
+        },
+        {
+            src: "/assets/images/image_3.jpeg",
+            scale: scale8,
+        },
+        {
+            src: "/assets/images/image_3.jpeg",
+            scale: scale8,
+        },
+    ];
+    return (
+        <div ref={container} className="h-[300vh] relative">
+            <div className="sticky top-0 h-screen flex justify-center items-center overflow-hidden p-4">
+                <motion.p
+                    style={{ opacity }}
+                    className="z-10 text-6xl font-semibold"
+                >
+                    Lorem, ipsum dolor.
+                </motion.p>
+                {pictures.map(({ src, scale }, i) => {
+                    return (
+                        <motion.div
+                            className="w-full h-full absolute top-0 flex items-center justify-center"
+                            style={{ scale }}
+                        >
+                            <div
+                                className={cn(
+                                    "h-[25vh] w-[25vw] rounded-lg overflow-hidden  relative",
+                                    i == 1 &&
+                                        "-top-[33vh] right-[5vw] h-[30vh] w-[35vw]",
+                                    i == 2 &&
+                                        "-top-[16vh] left-[25vw] h-[60vh] w-[20vw]",
+                                    i == 3 && "-left-[27vw] h-[28vh] w-[25vw] ",
+                                    i == 4 &&
+                                        "-top-[28vh] -left-[32vw] h-[18vh] w-[15vw]",
+                                    i == 5 &&
+                                        "-bottom-[30vh] -right-[15vw]  h-[25vh] w-[30vw] ",
+                                    i == 6 &&
+                                        "-bottom-[30vh]  -left-[15vw] h-[25vh] w-[25vw]"
+                                )}
+                            >
+                                <img
+                                    src={src}
+                                    alt="image"
+                                    className="object-cover w-full h-full "
+                                />
+                            </div>
+                        </motion.div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default ExploreZoomParallax;
