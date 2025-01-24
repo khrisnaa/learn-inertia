b2cc46166356dabef9c5e8683b525db9863f2aad
khrisnaa <igdkhrisnaaa@gmail.com>
2025-01-24 20:46:12 +0800

    feat: add contact page

--- resources/js/Pages/Client/Contact.jsx
+++ resources/js/Pages/Client/Contact.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ContactForm from "./Partials/ContactForm";
+import ClientLayout from "@/Layouts/ClientLayout";
+import ContactSection from "./Partials/ContactSection";
 
 const Contact = () => {
-    return <div>Contact</div>;
+    return (
+        <ClientLayout>
+            <ContactForm />
+            <ContactSection />
+        </ClientLayout>
+    );
 };
 
 export default Contact;
